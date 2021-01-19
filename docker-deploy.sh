dcae521b170ca17d633615c4c98dab5383492fe5
Deployment/mbreese <noreply@compgen.io>
2021-01-19 22:52:33 +0000

    deploy

---
+++ docker-deploy.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+bundle update && bundle exec jekyll build
+
+if [ $TOKEN != "" ]; then
+git config user.email "noreply@compgen.io"
+git config user.name "Deployment/$GITHUB_ACTOR"
+fi
+
+git submodule sync --recursive
+git submodule update --remote --init --force --recursive
+git commit -am 'submodule update'
+
+cd modules/cgpipe-docs
+bundle update && bundle exec jekyll build
+cd ../..
+
+cd modules/tabl-docs
+bundle update && bundle exec jekyll build
+
+cd ../../_site/
+echo "compgen.io" > CNAME
+
+rm cgpipe.html
+mkdir cgpipe
+cp -R ../modules/cgpipe-docs/_site/* cgpipe/
+
+rm tabl.html
+mkdir tabl
+cp -R ../modules/tabl-docs/_site/* tabl/
+
+
+git init
+if [ $TOKEN != "" ]; then
+git config user.email "noreply@compgen.io"
+git config user.name "Deployment/$GITHUB_ACTOR"
+fi
+git add .
+git commit -m 'deploy'
+if [ $TOKEN != "" ]; then
+    git remote add origin https://deploy:$TOKEN@github.com/compgen-io/compgen-io-site.git
+else
+    git remote add origin git@github.com:compgen-io/compgen-io-site.git
+fi
+git checkout -b gh-pages
+git push --force origin gh-pages
+cd ..
+
+
